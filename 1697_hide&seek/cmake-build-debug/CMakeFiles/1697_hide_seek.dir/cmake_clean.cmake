3bb8b09d8d0f4905817797e71dc76727f1d0f8c9
HyeokSoo Lee <lys8325@naver.com>
2019-11-11 00:36:48 +0900

    1

---
+++ 1697_hide&seek/cmake-build-debug/CMakeFiles/1697_hide_seek.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1697_hide_seek.dir/main.cpp.o"
+  "1697_hide_seek.pdb"
+  "1697_hide_seek"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1697_hide_seek.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
