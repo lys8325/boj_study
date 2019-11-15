7e46bbeb850ec133beb532e9f7d602657b58491b
HyeokSoo Lee <lys8325@naver.com>
2019-11-15 19:59:02 +0900

    1

---
+++ 12764_pc/cmake-build-debug/CMakeFiles/12764_pc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/12764_pc.dir/main.cpp.o"
+  "12764_pc.pdb"
+  "12764_pc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/12764_pc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 12764_pc/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+int main() {
+    int a=1<<1;
+    printf("%d",a);
+    return 0;
+}
