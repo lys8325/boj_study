af111cb1ac6e853d4d6c4decac8e1c0a88edddec
LeeHyeokSoo <lys8325@naver.com>
2020-09-27 14:41:23 +0900

    fail..

---
+++ 01939/cmake-build-debug/CMakeFiles/01939.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "01939"
+  "01939.pdb"
+  "CMakeFiles/01939.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01939.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 01939/main.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+using namespace std;
+int n, m, start, end, weight;
+int main() {
+    scanf("%d %d", &n, &m);
+    for(int i=0;i<m;++i){
+        scanf("%d %d %d", &start, &end, &weight);
+    }
+    return 0;
+}
