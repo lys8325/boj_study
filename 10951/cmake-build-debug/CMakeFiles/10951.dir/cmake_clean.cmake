1ed13082e776d100b4fae5cc9df2c9dc7f6b032c
LeeHyeokSoo <lys8325@naver.com>
2020-10-09 18:10:07 +0900

    A+B - 4

---
+++ 10951/cmake-build-debug/CMakeFiles/10951.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "10951"
+  "10951.pdb"
+  "CMakeFiles/10951.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10951.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
