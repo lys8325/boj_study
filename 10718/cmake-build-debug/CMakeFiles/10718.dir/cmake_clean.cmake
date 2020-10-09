b20956eeb586095bdc63e802acded6d73b19cc46
LeeHyeokSoo <lys8325@naver.com>
2020-10-09 16:25:23 +0900

    We love kriii

---
+++ 10718/cmake-build-debug/CMakeFiles/10718.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "10718"
+  "10718.pdb"
+  "CMakeFiles/10718.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10718.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 10718/main.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+using namespace std;
+
+int main() {
+    char *str = "강한친구 대한육군";
+    printf("%s\n%s", str, str);
+    return 0;
+}
