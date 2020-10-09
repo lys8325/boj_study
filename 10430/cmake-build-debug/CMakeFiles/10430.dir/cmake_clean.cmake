400ae9a28f40d4001570d0895efb33a4475c8792
LeeHyeokSoo <lys8325@naver.com>
2020-10-09 16:42:47 +0900

    We love kriii

---
+++ 10430/cmake-build-debug/CMakeFiles/10430.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "10430"
+  "10430.pdb"
+  "CMakeFiles/10430.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10430.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
