2e48c62a876fac606fc35dca422bc02fde214690
LeeHyeokSoo <lys8325@naver.com>
2020-10-09 16:14:58 +0900

    Hello World

---
+++ 02557/cmake-build-debug/CMakeFiles/02557.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "02557"
+  "02557.pdb"
+  "CMakeFiles/02557.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/02557.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
