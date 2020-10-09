5f14e517f52d058828bced04465525fafae594ed
LeeHyeokSoo <lys8325@naver.com>
2020-10-09 17:23:52 +0900

    알람 시계

---
+++ 02884/cmake-build-debug/CMakeFiles/02884.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "02884"
+  "02884.pdb"
+  "CMakeFiles/02884.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/02884.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
