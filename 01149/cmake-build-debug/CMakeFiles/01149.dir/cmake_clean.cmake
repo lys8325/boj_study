c5e64a3a8ea313b4b6fdaa8cac4cef120a5c68d0
LeeHyeokSoo <lys8325@naver.com>
2020-10-18 16:54:50 +0900

    RGB거리

---
+++ 01149/cmake-build-debug/CMakeFiles/01149.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "01149"
+  "01149.pdb"
+  "CMakeFiles/01149.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01149.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 01149/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int rgb[1000][3];
+int memo[1000][3];
+
+int dp(int idx, int pre){
+    int &ret = memo[idx][];
+    if(idx == n){
+        return v;
+    }
+
+    if(memo[idx] != 0){
+        return memo[idx];
+    }
+
+    for(int i=0;i<3;++i){
+        for(i == idx){
+            continue;
+        }
+        if(memo[idx][i])
+    }
+}
+
+int main() {
+    int r, g, b;
+
+    cin>>n;
+    for(int i=0;i<n;++i){
+        cin>>rgb[i][0]>>rgb[i][1]>>rgb[i][2];
+    }
+    return 0;
+}
