2d74e391eb72362052fa10cc355368fe88e956c8
LeeHyeokSoo <lys8325@naver.com>
2020-10-22 22:09:12 +0900

    RGB거리

--- 01149/main.cpp
+++ 01149/main.cpp
@@ -1,35 +1,51 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <map>
 using namespace std;
-i
+
 int n;
 int rgb[1000][3];
-int memo[1000][4];
+int memo[1000][3];
 
-int dp(int idx, int pre){
-    int &ret = memo[idx][3];
-    if(idx == n){
-        return v;
+int dp(int idx, int color){
+    int &minV = memo[idx][color];
+
+    if(idx == 0){
+        minV = rgb[idx][color];
+        return minV;
     }
 
-    if(memo[idx] != 0){
-        return memo[idx];
+    if(memo[idx][color] != 0){
+        return minV;
     }
 
-    for(int i=0;i<3;++i){
-        for(i == idx){
-            continue;
-        }
-        
+    if(color == 0){
+        minV = min(dp(idx-1,1), dp(idx-1,2)) + rgb[idx][0];
+    }else if(color == 1){
+        minV = min(dp(idx-1,0), dp(idx-1,2)) + rgb[idx][1];
+    }else{
+        minV = min(dp(idx-1,0), dp(idx-1,1)) + rgb[idx][2];
     }
+
+    return minV;
 }
 
 int main() {
-    int r, g, b;
+    int minR, minG, minB, ans;
 
     cin>>n;
     for(int i=0;i<n;++i){
         cin>>rgb[i][0]>>rgb[i][1]>>rgb[i][2];
     }
+
+    minR = dp(n-1,0);
+    minG = dp(n-1,1);
+    minB = dp(n-1,2);
+
+    ans = min(minR, minG);
+    ans = min(ans, minB);
+    cout<<ans;
+
     return 0;
 }
