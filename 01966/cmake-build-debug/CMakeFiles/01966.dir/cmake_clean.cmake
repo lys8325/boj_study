e7080ec36bfa426b1894fe1c7f778fa7bfdb2f64
LeeHyeokSoo <lys8325@naver.com>
2020-11-02 23:54:57 +0900

    q

---
+++ 01966/cmake-build-debug/CMakeFiles/01966.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "01966"
+  "01966.pdb"
+  "CMakeFiles/01966.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01966.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
