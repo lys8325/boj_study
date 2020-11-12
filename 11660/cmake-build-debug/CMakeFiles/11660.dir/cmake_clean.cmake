0736743a9caee9a5c0637a5bcd5feca2ff576a2b
LeeHyeokSoo <lys8325@naver.com>
2020-11-13 08:39:29 +0900

    구간 합 구하기 5

---
+++ 11660/cmake-build-debug/CMakeFiles/11660.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "11660"
+  "11660.pdb"
+  "CMakeFiles/11660.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11660.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 11660/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int board[1025][1025];
+int memo[1025][1025];
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
