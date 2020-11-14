0c64e95f23f145c2da901348cbdf2f5c3b6715a7
LeeHyeokSoo <lys8325@naver.com>
2020-11-14 19:50:03 +0900

    구간 합 구하기 5

--- 11660/main.cpp
+++ 11660/main.cpp
@@ -4,7 +4,45 @@ using namespace std;
 int board[1025][1025];
 int memo[1025][1025];
 
+int dp(int x, int y){
+    int &v = memo[x][y];
+
+    if(y == 0){
+        return 0;
+    }
+
+    if(v != 0){
+        return v;
+    }
+
+    for(int i=1;i<=y;++i){
+        v += board[x][i];
+    }
+
+    return v;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int n, m, x1, x2, y1, y2, sum;
+    cin>>n>>m;
+
+    for(int i=1;i<=n;++i){
+        for(int j=1;j<=n;++j){
+            cin>>board[i][j];
+        }
+    }
+
+    for(int i=0;i<m;++i){
+        cin>>x1>>y1>>x2>>y2;
+
+        sum = 0;
+        for(int j=x1;j<=x2;++j){
+            sum += (dp(j, y2) - dp(j, y1-1));
+        }
+        cout<<sum<<"\n";
+    }
     return 0;
 }
