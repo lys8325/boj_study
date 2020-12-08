edc146428ea842e572f84e708164c975addc27d9
LeeHyeokSoo <lys8325@naver.com>
2020-12-09 01:51:56 +0900

    최소 힙

---
+++ 01927/cmake-build-debug/CMakeFiles/01927.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "01927"
+  "01927.pdb"
+  "CMakeFiles/01927.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01927.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 01927/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+vector<int> v(1,0);
+int n, heapSize;
+
+void heapify(int idx){
+    if(heapSize == 0 || idx == 0){
+        return;
+    }
+
+    int nextIdx = 2 * idx;
+    if(nextIdx > heapSize){
+        return;
+    }
+
+    if(nextIdx+1 <= heapSize){
+        if(v[nextIdx] > v[nextIdx+1]){
+            ++nextIdx;
+        }
+    }
+
+    if(v[idx] > v[nextIdx]){
+        int tmp = v[idx];
+        v[idx] = v[nextIdx];
+        v[nextIdx] = tmp;
+
+        heapify(nextIdx);
+    }
+}
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int num, tmp;
+    priority_queue<int> pq;
+
+    cin>>n;
+    for(int i=0;i<n;++i){
+        cin>>num;
+
+        if(num == 0){
+            if(heapSize != 0){
+                cout<<v[1]<<"\n";
+
+                tmp = v[1];
+                v[1] = v[heapSize];
+                v[heapSize] = tmp;
+                v.pop_back();
+                --heapSize;
+                heapify(1);
+            }else{
+                cout<<0<<"\n";
+            }
+//            if(pq.empty()){
+//                cout<<0<<"\n";
+//            }else{
+//                cout<<pq.top()<<"\n";
+//                pq.pop();
+//            }
+        }else{
+            v.push_back(num);
+            ++heapSize;
+            heapify(heapSize/2);
+//            pq.push(num);
+        }
+    }
+    return 0;
+}
