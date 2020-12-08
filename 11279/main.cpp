9870e89f68f1edb78664951da37165bce3f237d0
LeeHyeokSoo <lys8325@naver.com>
2020-12-08 23:58:25 +0900

    최대 힙

--- 11279/main.cpp
+++ 11279/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 vector<int> v(1,0);
@@ -34,28 +35,36 @@ int main() {
     ios::sync_with_stdio(false);
 
     int num, tmp;
+    priority_queue<int> pq;
 
     cin>>n;
     for(int i=0;i<n;++i){
         cin>>num;
 
         if(num == 0){
-            if(heapSize){
-                cout<<v[1]<<"\n";
-
-                tmp = v[1];
-                v[1] = v[heapSize];
-                v[heapSize] = tmp;
-                v.pop_back();
-                --heapSize;
-                heapify(1);
-            }else{
+//            if(heapSize == 0){
+//                cout<<v[1]<<"\n";
+//
+//                tmp = v[1];
+//                v[1] = v[heapSize];
+//                v[heapSize] = tmp;
+//                v.pop_back();
+//                --heapSize;
+//                heapify(1);
+//            }else{
+//                cout<<0<<"\n";
+//            }
+            if(pq.empty()){
                 cout<<0<<"\n";
+            }else{
+                cout<<pq.top()<<"\n";
+                pq.pop();
             }
         }else{
-            v.push_back(num);
-            ++heapSize;
-            heapify(heapSize/2);
+//            v.push_back(num);
+//            ++heapSize;
+//            heapify(heapSize/2);
+            pq.push(num);
         }
     }
     return 0;
