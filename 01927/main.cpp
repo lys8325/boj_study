62babfd7ced8f6c9fa448462c6234939a439e206
LeeHyeokSoo <lys8325@naver.com>
2020-12-10 01:53:56 +0900

    최소 힙

--- 01927/main.cpp
+++ 01927/main.cpp
@@ -35,36 +35,36 @@ int main() {
     ios::sync_with_stdio(false);
 
     int num, tmp;
-    priority_queue<int> pq;
+    priority_queue<int, vector<int>, greater<int>> pq;
 
     cin>>n;
     for(int i=0;i<n;++i){
         cin>>num;
 
         if(num == 0){
-            if(heapSize != 0){
-                cout<<v[1]<<"\n";
-
-                tmp = v[1];
-                v[1] = v[heapSize];
-                v[heapSize] = tmp;
-                v.pop_back();
-                --heapSize;
-                heapify(1);
-            }else{
-                cout<<0<<"\n";
-            }
-//            if(pq.empty()){
-//                cout<<0<<"\n";
+//            if(heapSize != 0){
+//                cout<<v[1]<<"\n";
+//
+//                tmp = v[1];
+//                v[1] = v[heapSize];
+//                v[heapSize] = tmp;
+//                v.pop_back();
+//                --heapSize;
+//                heapify(1);
 //            }else{
-//                cout<<pq.top()<<"\n";
-//                pq.pop();
+//                cout<<0<<"\n";
 //            }
+            if(pq.empty()){
+                cout<<0<<"\n";
+            }else{
+                cout<<pq.top()<<"\n";
+                pq.pop();
+            }
         }else{
-            v.push_back(num);
-            ++heapSize;
-            heapify(heapSize/2);
-//            pq.push(num);
+//            v.push_back(num);
+//            ++heapSize;
+//            heapify(heapSize/2);
+            pq.push(num);
         }
     }
     return 0;
