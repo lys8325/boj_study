1e5ada98527c1b7e048e2e62f65f5fda936d8e9e
LeeHyeokSoo <lys8325@naver.com>
2020-12-12 23:39:35 +0900

    가운데를 말해요

---
+++ 01655/cmake-build-debug/CMakeFiles/01655.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "01655"
+  "01655.pdb"
+  "CMakeFiles/01655.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01655.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
