1e1fdf0998744cc200e9f8bc41a75d65f088a9e1
LeeHyeokSoo <lys8325@naver.com>
2020-12-13 23:59:16 +0900

    가운데를 말해요

--- 01655/main.cpp
+++ 01655/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <queue>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+    int n, mid, num;
+    cin>>n>>mid;
+    cout<<mid<<"\n";
+    for(int i=1;i<n;++i){
+        cin>>num;
+        if(num < mid){
+            maxHeap.push(num);
+        }else{
+            minHeap.push(num);
+        }
+
+        if(maxHeap.size() > minHeap.size()){
+            minHeap.push(mid);
+            mid = maxHeap.top();
+            maxHeap.pop();
+        }else if(maxHeap.size() > minHeap.size()){
+            maxHeap.push(mid);
+            mid = minHeap.top();
+            minHeap.pop();
+        }
+        cout<<mid<<"\n";
+    }
     return 0;
 }
