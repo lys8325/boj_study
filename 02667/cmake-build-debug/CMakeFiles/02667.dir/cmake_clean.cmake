a3694e0eb3768242b2546fa5e7c78381256b1591
LeeHyeokSoo <lys8325@naver.com>
2020-12-15 23:51:33 +0900

    단지번호붙이기

---
+++ 02667/cmake-build-debug/CMakeFiles/02667.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "02667"
+  "02667.pdb"
+  "CMakeFiles/02667.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/02667.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
