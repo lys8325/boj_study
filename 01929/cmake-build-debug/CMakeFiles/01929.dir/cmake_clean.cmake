26b7b8e6344bfc34379cb0a15d8f8ec9f4d23e7d
LeeHyeokSoo <lys8325@naver.com>
2020-12-17 23:53:55 +0900

    소수 구하기

---
+++ 01929/cmake-build-debug/CMakeFiles/01929.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "01929"
+  "01929.pdb"
+  "CMakeFiles/01929.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01929.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 01929/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, n;
+    bool isPrime[1000001] = {false};
+
+    cin>>m>>n;
+    for(int i=2;i<=n;++i){
+        isPrime[i] = true;
+    }
+
+    for(int i=2;i*i<=n;++i){
+        if(isPrime[i]){
+            for(int j=i*i;j<=n;j += i){
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    for(int i=m;i<=n;++i){
+        if(isPrime[i]){
+            cout<<i<<"\n";
+        }
+    }
+    return 0;
+}
