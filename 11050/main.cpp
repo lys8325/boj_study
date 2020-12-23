e2f74a26adfb1da1d9641d2cbfa4aace6e93d0c4
LeeHyeokSoo <lys8325@naver.com>
2020-12-24 00:05:59 +0900

    이항 계수 1

--- 11050/main.cpp
+++ 11050/main.cpp
@@ -4,17 +4,30 @@ using namespace std;
 int memo[11][11];
 
 int calc(int a, int b){
+    int &v = memo[a][b];
     if(b == 1){
-        return a;
+        v = a;
     }
 
-    return calc(a-1, b) + calc(a-1, b-1);
+    if(a == b){
+        v = 1;
+    }
+
+    if(v != 0){
+        return v;
+    }
+    v = calc(a-1, b) + calc(a-1, b-1);
+    return v;
 }
 
 int main() {
     int n, k;
 
     cin>>n>>k;
-    cout<<calc(n, k);
+    if(k == 0){
+        cout<<1;
+    }else{
+        cout<<calc(n, k);
+    }
     return 0;
 }
