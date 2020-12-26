49b411d9d329e6937a64522ed95d2df4f9854bcb
LeeHyeokSoo <lys8325@naver.com>
2020-12-26 23:59:25 +0900

    소수 찾기

---
+++ 01978/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, num, ans = 0;
+
+    cin>>n;
+    for(int i=0;i<n;++i){
+        cin>>num;
+        if(num == 1){
+            continue;
+        }
+
+        
+    }
+
+    return 0;
+}
