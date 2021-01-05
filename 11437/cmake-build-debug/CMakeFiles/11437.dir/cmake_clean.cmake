37d63541bdfa293775ec73814f7767b8efc69391
LeeHyeokSoo <lys8325@naver.com>
2021-01-05 21:05:20 +0900

    LCA

---
+++ 11437/cmake-build-debug/CMakeFiles/11437.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "11437"
+  "11437.pdb"
+  "CMakeFiles/11437.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11437.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
