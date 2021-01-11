3415b42f9190672418ecd66b835fc71398f80d6e
LeeHyeokSoo <lys8325@naver.com>
2021-01-11 23:59:27 +0900

    큐

---
+++ 10845/cmake-build-debug/CMakeFiles/10845.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "10845"
+  "10845.pdb"
+  "CMakeFiles/10845.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10845.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 10845/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    queue<int> q;
+    int n, num;
+    string op;
+
+    cin>>n;
+    for(int i=0;i<n;++i){
+        cin>>op;
+        if(op == "push"){
+            cin>>num;
+            q.push(num);
+        }else if(op == "pop"){
+            if(q.empty()){
+                cout<<"-1\n";
+            }else{
+                cout<<q.front()<<"\n";
+                q.pop();
+            }
+        }else if(op == "size"){
+            
+        }
+    }
+    return 0;
+}
