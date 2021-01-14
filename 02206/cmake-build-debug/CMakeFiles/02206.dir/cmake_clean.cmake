04db9f85ca245ea83ae53a7e5cc8e0a7fcfe1143
LeeHyeokSoo <lys8325@naver.com>
2021-01-14 23:35:44 +0900

    벽 부수고 이동하기

---
+++ 02206/cmake-build-debug/CMakeFiles/02206.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "02206"
+  "02206.pdb"
+  "CMakeFiles/02206.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/02206.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
