2a264bfeb9d7e3f8b041af34f75d89e55b9c855b
LeeHyeokSoo <lys8325@naver.com>
2021-01-19 23:57:01 +0900

    도시 분할 계획

---
+++ 01647/cmake-build-debug/CMakeFiles/01647.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "01647"
+  "01647.pdb"
+  "CMakeFiles/01647.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01647.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
