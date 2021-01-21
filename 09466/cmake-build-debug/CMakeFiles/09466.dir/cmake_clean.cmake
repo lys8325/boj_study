c93beb543b48ce1acfb7b7a444dc1373360fa84f
LeeHyeokSoo <lys8325@naver.com>
2021-01-21 23:49:29 +0900

    텀 프로젝트

---
+++ 09466/cmake-build-debug/CMakeFiles/09466.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "09466"
+  "09466.pdb"
+  "CMakeFiles/09466.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/09466.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
