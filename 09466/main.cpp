acd99a8a84cd3b2ee7053855955f98d6151261f9
LeeHyeokSoo <lys8325@naver.com>
2021-01-22 20:48:47 +0900

    텀 프로젝트

--- 09466/main.cpp
+++ 09466/main.cpp
@@ -1,6 +1,36 @@
-#include <iostream>
+#include<iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int t;
+
+int main(void){
+    cin >> t;
+    while (t--){
+        int n, student[100001], finish[100001], ans = 0;
+        bool check[100001];
+
+        cin >> n;
+        for (int i = 1; i <= n; i++){
+            check[i] = false;
+            finish[i] = 0;
+        }
+
+        for (int i = 1; i <= n; i++){
+            cin >> student[i];
+            finish[student[i]]++;
+        }
+
+        for (int i = 1; i <= n; i++){
+            int now = i;
+            while (!check[now] && !finish[now]){
+                ans++;
+                check[now] = true;
+                finish[student[now]]--;
+                now = student[now];
+            }
+        }
+
+        cout << ans << "\n";
+    }
     return 0;
 }
