3c3469607d1813eed54ed2db99876770166563f9
LeeHyeokSoo <lys8325@naver.com>
2021-01-23 21:31:24 +0900

    세 용액

---
+++ 02473/cmake-build-debug/CMakeFiles/02473.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "02473"
+  "02473.pdb"
+  "CMakeFiles/02473.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/02473.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
