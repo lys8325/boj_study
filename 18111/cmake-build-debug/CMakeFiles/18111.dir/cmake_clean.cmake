b0816bf8d1529b448580470491bd377b35d89186
LeeHyeokSoo <lys8325@naver.com>
2021-01-31 23:43:10 +0900

    마인크래프트

---
+++ 18111/cmake-build-debug/CMakeFiles/18111.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "18111"
+  "18111.pdb"
+  "CMakeFiles/18111.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/18111.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
