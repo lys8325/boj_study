cf4f9becdd09fa7287d32cf999086e9224251ed3
LeeHyeokSoo <lys8325@naver.com>
2021-02-01 23:40:26 +0900

    최종 순위

---
+++ 03665/cmake-build-debug/CMakeFiles/03665.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "03665"
+  "03665.pdb"
+  "CMakeFiles/03665.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/03665.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
