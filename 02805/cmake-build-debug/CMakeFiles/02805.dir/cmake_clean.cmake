9b7758d22fbd765e4895911b1e7abef5ad020f35
LeeHyeokSoo <lys8325@naver.com>
2021-02-02 23:48:33 +0900

    나무 자르기

---
+++ 02805/cmake-build-debug/CMakeFiles/02805.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "02805"
+  "02805.pdb"
+  "CMakeFiles/02805.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/02805.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
