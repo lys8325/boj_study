81ee8eed21cc5f5d7eabd3b3daf5106ddcddfee7
LeeHyeokSoo <lys8325@naver.com>
2021-02-04 23:56:17 +0900

    행렬 곱셈

---
+++ 02740/cmake-build-debug/CMakeFiles/02740.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "02740"
+  "02740.pdb"
+  "CMakeFiles/02740.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/02740.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
