b228af476f29feb3953e0551ad8cb6e155153329
LeeHyeokSoo <lys8325@naver.com>
2021-02-06 22:25:47 +0900

    트리의 지름

---
+++ 01167/cmake-build-debug/CMakeFiles/01167.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "01167"
+  "01167.pdb"
+  "CMakeFiles/01167.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01167.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
