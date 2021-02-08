27bc9d17c42e0a10180c01db85f76dcca9158071
LeeHyeokSoo <lys8325@naver.com>
2021-02-08 23:56:13 +0900

    나무 자르기

--- 02805/main.cpp
+++ 02805/main.cpp
@@ -1,37 +1,46 @@
 #include <iostream>
-#define MAX 1000001
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int tree[MAX];
-int s, e;
-
 int main(){
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
 
-    int n, m;
-    scanf("%d %d", &n, &m);
-    for(int i=0; i<n; i++){
-        scanf("%d", &tree[i]);
-        if(tree[i]>e) e=tree[i];
+    int n, m, tmp, h, d, sum, ans = -1;
+    vector<int> v;
+
+    cin>>n>>m;
+    for(int i=0;i<n;++i){
+        cin>>tmp;
+        v.push_back(tmp);
     }
+    sort(v.begin(), v.end());
 
-    while(e>s){
-        int mid=(s+e)/2;
-        long long sum=0;
-        for(int i=0; i<n; i++){
-            int temp=tree[i]-mid;
-            sum+=temp>0?temp:0;
+    int l = 0, r = v[n-1], mid;
+
+    while(l < r){
+        sum = 0;
+        mid = (l + r) / 2;
+        h = v[mid];
+
+        for(int i=mid;i<n;++i){
+            d = v[i] - h;
+            if(d > 0){
+                sum += d;
+            }
         }
 
-
-        if(sum>=m){
-            s=mid+1;
-        } else {
-            e=mid;
+        if(sum < m){
+            r = mid - 1;
+        }else{
+            l = mid + 1;
+            if(sum == m){
+                ans = max(ans, h);
+            }
         }
-
-
     }
 
-    printf("%d", e-1);
+    cout<<ans;
     return 0;
 }
