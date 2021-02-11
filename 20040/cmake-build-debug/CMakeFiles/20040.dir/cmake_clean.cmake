3e017bc83f11551314a37917f2316a74eac1ee19
LeeHyeokSoo <lys8325@naver.com>
2021-02-11 23:30:34 +0900

    cycle

---
+++ 20040/cmake-build-debug/CMakeFiles/20040.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "20040"
+  "20040.pdb"
+  "CMakeFiles/20040.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/20040.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
