b119f0bdfd2784a4b3ebb150b242b4e6dc069883
LeeHyeokSoo <lys8325@naver.com>
2021-02-18 23:53:52 +0900

    신나는 함수 실행

---
+++ 09184/cmake-build-debug/CMakeFiles/09184.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "09184"
+  "09184.pdb"
+  "CMakeFiles/09184.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/09184.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
