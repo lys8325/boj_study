4597f411992f52e3affe2c7679b00d6db64af298
LeeHyeokSoo <lys8325@naver.com>
2021-02-23 23:59:34 +0900

    ready

---
+++ 10867/cmake-build-debug/CMakeFiles/10867.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "10867"
+  "10867.pdb"
+  "CMakeFiles/10867.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10867.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
