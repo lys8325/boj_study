c0b044eb346cb1f9deb84a6824f32c23e61c4caf
LeeHyeokSoo <lys8325@naver.com>
2021-02-24 23:48:00 +0900

    중복 빼고 정렬하기

--- 10867/main.cpp
+++ 10867/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <set>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    set<int> s;
+    int n, num;
+
+    cin>>n;
+    for(int i=0;i<n;++i){
+        cin>>num;
+        s.insert(num);
+    }
+
+    for(auto i : s){
+        cout<<i<<" ";
+    }
     return 0;
 }
