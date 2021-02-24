b96a3939caeb8c6bc68b543220ccd013ba3ea9f1
LeeHyeokSoo <lys8325@naver.com>
2021-02-24 23:49:37 +0900

    좌표 정렬하기 2

--- 11651/main.cpp
+++ 11651/main.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
+#include <cstdio>
+#include <queue>
+using namespace std;
+
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+int n, x, y;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    scanf("%d", &n);
+    for(int i=0;i<n;++i){
+        scanf("%d %d", &x, &y);
+        pq.push({y, x});
+    }
+    for(int i=0;i<n;++i){
+        printf("%d %d\n", pq.top().second, pq.top().first);
+        pq.pop();
+    }
     return 0;
 }
