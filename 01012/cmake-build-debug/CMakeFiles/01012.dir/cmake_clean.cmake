079dc309e7ddfa9f40fdbc42a3472b724d5f3d5a
LeeHyeokSoo <lys8325@naver.com>
2021-02-25 13:39:27 +0900

    유기농 배추

---
+++ 01012/cmake-build-debug/CMakeFiles/01012.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "01012"
+  "01012.pdb"
+  "CMakeFiles/01012.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01012.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
