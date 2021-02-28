426a6f210488c7f1ab1c86dd3efa1fc8b394128d
LeeHyeokSoo <lys8325@naver.com>
2021-02-28 23:44:40 +0900

    미세먼지 안녕!

---
+++ 17144/cmake-build-debug/CMakeFiles/17144.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "17144"
+  "17144.pdb"
+  "CMakeFiles/17144.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/17144.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
