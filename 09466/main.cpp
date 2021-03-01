f669b21b8f2e5657c7f30ae001c8372d8a2a690c
LeeHyeokSoo <lys8325@naver.com>
2021-03-01 23:39:28 +0900

    텀 프로젝트

--- 09466/main.cpp
+++ 09466/main.cpp
@@ -1,66 +1,54 @@
 #include <iostream>
+#include <memory.h>
 #include <vector>
 using namespace std;
-#pragma disable(warning:4996)
 
-void dfs(vector<int>& _num_vec, vector<int>& _group, int _n) {
-    int group_num = 1;
-    vector<int> is_visited(_n + 1, 0);
-    vector<int> visited_node;
+vector<pair<int, int>> v;
+bool visit[100001];
+bool visitEnd[100001];
+int t, n, num, cnt;
 
-    for (int n_idx = 1; n_idx <= _n; n_idx++) {
-        // 그룹 번호가 있으면 건너 뛴다.
-        if (_group[n_idx] || is_visited[n_idx]) continue;
+void dfs(int cur, int seq){
+    visit[cur] = true;
+    v[cur].second = seq;
+    int nextP = v[cur].first;
 
-        visited_node.clear();
-        is_visited[n_idx] = 1;
-        visited_node.push_back(n_idx);
-        int now_node = _num_vec[n_idx];
-
-        // 방문하지 않은 노드이면서 그룹도 지정되어있지 않은 경우에만 갱신을 수행
-        while (is_visited[now_node] == 0 && _group[now_node] == 0) {
-            is_visited[now_node] = 1;
-            visited_node.push_back(now_node);
-            now_node = _num_vec[now_node];
+    if(!visit[nextP]){
+        dfs(nextP, seq+1);
+    }else{
+        if(!visitEnd[nextP]){
+            cnt += (seq - v[nextP].second + 1);
         }
-
-        // 순환 끝나고 시작과 끝이 같으면?
-        int visited_size = visited_node.size();
-        int vis_idx;
-        // 순환이 시작되는 부분을 찾는다
-        // 만약 순환이 시작되는 부분이 아니면 -1을 넣어주어
-        // 그룹을 지을 수 없다는 것을 표시
-        for (vis_idx = 0; vis_idx < visited_size; vis_idx++)
-            if (visited_node[vis_idx] != now_node) _group[visited_node[vis_idx]] = -1;
-            else break;
-
-        for (; vis_idx < visited_size; vis_idx++)
-            _group[visited_node[vis_idx]] = group_num;
-        group_num++;
-
     }
+
+    visitEnd[cur] = true;
 }
 
 int main() {
+    cin.tie(NULL);
     ios::sync_with_stdio(false);
-    int T;
-    cin >> T;
-    while (T--) {
-        int n;
-        cin >> n;
-        vector<int> num_vec(n+1,0);
-        vector<int> group(n+1,0);
-        for (int n_idx = 1; n_idx <= n; n_idx++)
-            cin >> num_vec[n_idx];
 
-        // dfs 수행
-        dfs(num_vec, group, n);
+    cin>>t;
+    for(int i=0;i<t;++i){
+        cin>>n;
+        memset(visit, false, 100001);
+        memset(visitEnd, false, 100001);
+        v.clear(); v.push_back({0, 0});
+        cnt = 0;
 
-        int answer = 0;
-        for (int n_idx = 1; n_idx <= n; n_idx++)
-            if (group[n_idx] < 0 ) answer++;
+        for(int j=1;j<=n;++j){
+            cin>>num;
+            v.push_back({num, 1});
+        }
 
-        cout << answer << "\n";
+        for(int j=1;j<=n;++j){
+            if(!visit[j]){
+                dfs(j, 1);
+            }
+        }
+
+        cout<<n-cnt<<"\n";
     }
+
     return 0;
 }
