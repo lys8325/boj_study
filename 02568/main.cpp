682e86f55181c1e9bc519d651bac7d62e99938d3
LeeHyeokSoo <lys8325@naver.com>
2021-03-04 20:29:34 +0900

    전깃줄 - 2

--- 02568/main.cpp
+++ 02568/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 #include <algorithm>
 using namespace std;
 
 int isMember[100001];
+
+struct line{
+    int from;
+    int to;
+};
+
+bool cmp(line a, line b){
+    return a.from < b.from;
+}
 
 int main() {
     cin.tie(NULL);
@@ -12,43 +22,43 @@ int main() {
     int n, from, to, ansVal = 0, ansIdx = 0;
     cin>>n;
 
-    vector<pair<int, int>> v(n);
-    vector<pair<int, int>> num(n);
+    vector<line> num;
+    vector<int> v;
+    vector<int> seq;
+    stack<int> s;
 
     for(int i=0;i<n;++i){
         cin>>from>>to;
-        num[i] = {from, to};
+        num.push_back({from, to});
     }
-    sort(num.begin(), num.end());
+    sort(num.begin(), num.end(), cmp);
 
+    v.push_back(0);
     for(int i=0;i<n;++i){
-        v[i] = {1, i};
-        for(int j=0;j<i;++j){
-            if(num[i].second > num[j].second){
-                if(v[i].first < v[j].first + 1){
-                    v[i].first = v[j].first + 1;
-                    v[i].second = j;
-                }
-            }
-        }
-        if(ansVal < v[i].first){
-            ansVal = v[i].first;
-            ansIdx = i;
+        if(v.back() < num[i].to){
+            v.push_back(num[i].to);
+            seq.push_back(v.size()-1);
+        }else{
+            auto iter = lower_bound(v.begin(), v.end(), num[i].to);
+            *iter = num[i].to;
+            seq.push_back(iter - v.begin());
         }
     }
 
-    int tmpIdx = ansIdx;
-    while(tmpIdx != v[tmpIdx].second){
-        isMember[tmpIdx] = true;
-        tmpIdx = v[tmpIdx].second;
-    }
-    isMember[tmpIdx] = true;
+    int ans = v.size()-1;
+    cout<<n - ans<<"\n";
 
-    cout<<n - ansVal<<"\n";
-    for(int i=0;i<ansIdx;++i){
-        if(!isMember[i]){
-            cout<<num[i].first<<"\n";
+    for(int i=n-1;i>=0;--i){
+        if(seq[i] == ans){
+            --ans;
+        }else{
+            s.push(i);
         }
+    }
+
+    while(!s.empty()){
+        cout<<num[s.top()].from<<"\n";
+        s.pop();
     }
     return 0;
 }
