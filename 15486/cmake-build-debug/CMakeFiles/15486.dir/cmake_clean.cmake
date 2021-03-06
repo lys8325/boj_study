fa226cff4642c1249fff2dacd5731a8957726a95
LeeHyeokSoo <lys8325@naver.com>
2021-03-06 17:12:50 +0900

    퇴사 2

---
+++ 15486/cmake-build-debug/CMakeFiles/15486.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "15486"
+  "15486.pdb"
+  "CMakeFiles/15486.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/15486.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
