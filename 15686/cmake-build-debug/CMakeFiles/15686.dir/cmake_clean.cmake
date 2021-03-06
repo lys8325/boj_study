5b1a32777616588f917e961b4fc32712adc931d4
LeeHyeokSoo <lys8325@naver.com>
2021-03-06 21:23:20 +0900

    치킨 배달

---
+++ 15686/cmake-build-debug/CMakeFiles/15686.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "15686"
+  "15686.pdb"
+  "CMakeFiles/15686.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/15686.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
