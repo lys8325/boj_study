663a03e9b77459242c75e4f9814e81b1700e1b13
LeeHyeokSoo <lys8325@naver.com>
2021-03-07 19:31:49 +0900

    2xn 타일링

---
+++ 11726/cmake-build-debug/CMakeFiles/11726.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "11726"
+  "11726.pdb"
+  "CMakeFiles/11726.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11726.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
