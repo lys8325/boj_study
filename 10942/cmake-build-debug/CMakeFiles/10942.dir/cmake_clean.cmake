2617f6593dc82280d931551e18a257ce73c4a7ee
LeeHyeokSoo <lys8325@naver.com>
2021-03-08 23:33:30 +0900

    팰린드롬?

---
+++ 10942/cmake-build-debug/CMakeFiles/10942.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "10942"
+  "10942.pdb"
+  "CMakeFiles/10942.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10942.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
