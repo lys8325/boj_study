470ccd3be2610b63f1d372e7ce0496c4658d7f5d
LeeHyeokSoo <lys8325@naver.com>
2021-03-13 23:09:16 +0900

    순열 조합 구현

--- 10974/main.cpp
+++ 10974/main.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 int num[9];
 int permutation[9];
+int combination[9];
 bool used[9];
-int n;
+int n, pCnt, cCnt;
 
 void getPermutation(int len){
     if(len == n){
+        ++pCnt;
         for(int idx=1;idx<=n;++idx){
             cout<<permutation[idx]<<" ";
         }
@@ -15,7 +17,7 @@ void getPermutation(int len){
         return;
     }
 
-    for(int idx=1;idx<=n;++idx){
+    for(int idx=1;idx<=8;++idx){
         if(used[idx]){
             continue;
         }
@@ -27,19 +29,40 @@ void getPermutation(int len){
     }
 }
 
+void getCombination(int pos, int len){
+    if(len == n){
+        ++cCnt;
+        for(int idx=1;idx<=n;++idx){
+            cout<<combination[idx]<<" ";
+        }
+        cout<<"\n";
+        return;
+    }
+
+    for(int idx=pos;idx<=8;++idx){
+        combination[len+1] = num[idx];
+        getCombination(idx+1, len+1);
+    }
+}
+
 int main() {
     cin>>n;
 
-    for(int idx=1;idx<=n;++idx){
+    for(int idx=1;idx<=8;++idx){
         num[idx] = idx;
     }
 
-    for(int idx=1;idx<=n;++idx){
+    for(int idx=1;idx<=8;++idx){
         used[idx] = true;
         permutation[1] = num[idx];
         getPermutation(1);
         used[idx] = false;
     }
-
+    cout<<pCnt<<"\n";
+    for(int idx=1;idx<=8;++idx){
+        combination[1] = num[idx];
+        getCombination(idx+1, 1);
+    }
+    cout<<cCnt<<"\n";
     return 0;
 }
