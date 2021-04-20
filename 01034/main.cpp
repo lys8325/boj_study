8307672afa601ad6fcb5f84a83c83465034be779
LeeHyeokSoo <lys8325@naver.com>
2021-04-20 22:17:49 +0900

    램프

--- 01034/main.cpp
+++ 01034/main.cpp
@@ -4,77 +4,37 @@
 using namespace std;
 
 string lamp[50];
-int onCnt[50];
-int n, m, k, answer = -1;
-
-int check(){
-    int cnt = 0;
-
-    for(int i=0;i<n;++i){
-        if(onCnt[i] == m){
-            ++cnt;
-        }
-    }
-
-    return cnt;
-}
-
-void recover(int col){
-    for(int i=0;i<n;++i){
-        if(lamp[i][col] == '1'){
-            lamp[i][col] = '0';
-            --onCnt[i];
-        }else{
-            lamp[i][col] = '1';
-            ++onCnt[i];
-        }
-    }
-}
-
-void click(int col, int cnt){
-    for(int i=0;i<n;++i){
-        if(lamp[i][col] == '1'){
-            lamp[i][col] = '0';
-            --onCnt[i];
-        }else if(lamp[i][col] == '0'){
-            lamp[i][col] = '1';
-            ++onCnt[i];
-        }
-    }
-
-    if(cnt == k){
-        answer = max(answer, check());
-        return;
-    }
-
-    for(int i=0;i<m;++i){
-        click(i, cnt+1);
-        recover(i);
-    }
-}
+int offCnt[50];
 
 int main() {
     cin.tie(NULL);
     ios::sync_with_stdio(false);
 
-    int cnt;
+    int n, m, k, answer = 0, cnt;
 
     cin>>n>>m;
     for(int i=0;i<n;++i){
         cin>>lamp[i];
         cnt = 0;
         for(int j=0;j<m;++j){
-            if(lamp[i][j] == '1'){
+            if(lamp[i][j] == '0'){
                 ++cnt;
             }
         }
-        onCnt[i] = cnt;
+        offCnt[i] = cnt;
     }
 
     cin>>k;
-    for(int i=0;i<m;++i){
-        click(i, 1);
-        recover(i);
+    for(int i=0;i<n;++i){
+        if(offCnt[i] == k || (offCnt[i] < k && (k-offCnt[i]) % 2 == 0)){
+            cnt = 0;
+            for(int j=0;j<n;++j){
+                if(lamp[i] == lamp[j]){
+                    ++cnt;
+                }
+            }
+            answer = max(answer, cnt);
+        }
     }
 
     cout<<answer;
